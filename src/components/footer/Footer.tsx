084942a2065c51b3b8ca6418521a4291074f4dbd
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2021-09-27 18:05:33 +0530

    Added responsive footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,6 +6,9 @@ export const Footer = () => {
             <div className="footer-content">
                 A <span className="footer-content__fancy">fancy</span> footer will appear right here
             </div>
+            <div className="footer-content__small">
+                <span className="footer-content__fancy__small">Fancy</span> footer here
+            </div>
         </footer>
     )
 }
